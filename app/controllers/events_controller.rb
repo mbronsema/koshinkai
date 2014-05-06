83651eee09fecf102de4ebb3c33181134696e3f6
Martin <martin.bronsema@gmail.com>
2014-05-06 14:58:23 +0200

    event controller
    update event controller not yet done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,46 @@
 class EventsController < ApplicationController
+
+	def index
+		@event = Event.all
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+
+		if @event.save
+			redirect_to @event
+		else
+			render 'new'
+		end
+
+	end
+
+	def edit 
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+	end
+
+	def delete
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:title, :message, :starting_at, :ending_at :)
+	end
 end
