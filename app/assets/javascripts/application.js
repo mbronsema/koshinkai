f5c5b74288da6431adeb52673707b4565d5012b5
Martin <martin.bronsema@gmail.com>
2014-05-12 11:18:49 +0200

    postcontroller content added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,5 +31,6 @@ $(document).ready(function() {
     			textColor: 'white'
        		}
     	]
+    
     });
 });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,51 @@
 class PostsController < ApplicationController
+
+ respond_to :html, :json
+  def index
+    @posts = Post.all
+    respond_with @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    respond_with @post
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit 
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :prive, :user_id, category_id, event_id)
+  end
+
 end
