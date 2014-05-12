630e0749819f7c5b76faa93e4954b80fc7876d14
Martin <martin.bronsema@gmail.com>
2014-05-12 15:00:50 +0200

    catergory stuff added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505204109) do
+ActiveRecord::Schema.define(version: 20140512125739) do
+
+  create_table "categories", force: true do |t|
+    t.string   "menuname"
+    t.string   "submenuname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.string   "title"
