5730be35f23d2c7fad31a8e0326a3a5832bf9d6d
martinpetrusma <martinpok@hotmail.com>
2014-05-15 17:26:22 +0200

    devise authentication + enum roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,31 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+   before_action :authenticate_user!
   before_filter :fetch_menu
+
+
   protect_from_forgery with: :exception
 	before_action :set_locale
 		def set_locale
 			I18n.locale = params[:locale] || I18n.default_locale
 		end
 
+
    def fetch_menu
     @menu = Category.all
     @mainmenu = {'Home' => root_path, 'Prijzen' => '#', 'Aikido' => '#','Koshinkai' => '#', 'Agenda' => events_path, 'Media' => '#', 'Contant' => '#'}
+  end
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
+  
+  def user_not_authorized
+    flash[:alert] = "Access denied."
+    redirect_to (request.referrer || root_path)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+                
   has_many :posts
   has_many :events
+  
 end
