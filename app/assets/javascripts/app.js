cecd7ba27188e6bb2fa8d9dccea3cf6cfdef0a32
jorenmartijn <jorenmartijn@rc11539.lan.nhl.nl>
2014-06-10 11:08:15 +0200

    Calendar geupdated naar laatste versie
    Moet je gem update doen, misschien eerst ook wel de huidige gem
    verwijderen en dan gem update

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -34,33 +34,41 @@ $(document).ready(function() {
 		language: 'nl'
 	});
 
-    // Initialise the fullcalendar
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,basicWeek,basicDay'
-        },
-        defaultView: 'basicWeek',
-        eventClick: function(calEvent, jsEvent, view) {
-            var loc = self.location;
-            console.log(calEvent);
-            console.log(jsEvent);
-            console.log(view);
-            window.location = "http://localhost:3000/events/" + calEvent.id;
-        },
-    	eventSources:[
-    		{    			
+    $("#calendar").fullCalendar({
+        eventSources: [
+
+            // your event source
+            {
                 url: '/events',
-    			type: 'GET',
-    			error: function(){
-    				alert('couldn\'t find the feed')
-    			},
-    			color: '#108ac2',
-    			textColor: 'white'
-       		}
-    	]
-    
+                type: 'GET',
+                error: function() {
+                    alert('there was an error while fetching events!');
+                },
+                color: 'yellow',   // a non-ajax option
+                textColor: 'black' // a non-ajax option
+            }
+
+            // any other sources...
+
+        ]
     });
+    // Initialise the fullcalendar
+    // $('#calendar').fullCalendar({
+    //     header: {
+    //         left: 'prev,next today',
+    //         center: 'title',
+    //         right: 'month,basicWeek,basicDay'
+    //     },
+    //     defaultView: 'basicWeek',
+    //     eventClick: function(calEvent, jsEvent, view) {
+    //         var loc = self.location;
+    //         console.log(calEvent);
+    //         console.log(jsEvent);
+    //         console.log(view);
+    //         window.location = "http://localhost:3000/events/" + calEvent.id;
+    //     },
+    //     events: '/events'
+    
+    // });
     $(".current-page-item").parents('li, ul', this).addClass('current-page-item');
 });
--- app/views/events/index.json.jbuilder
+++ app/views/events/index.json.jbuilder
@@ -2,7 +2,8 @@ json.(@events) do |event|
   json.id event.id
   json.title event.title
   json.message event.message
-  json.start event.starting_at.to_i
-  json.end event.ending_at.to_i
+  json.url url_for(edit_event_path(event.id))
+  json.start event.starting_at
+  json.end event.ending_at
 end
 
