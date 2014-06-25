dd4abbc6291956cfb5d399c272b3757698d75bd0
Martin <martin.bronsema@gmail.com>
2014-06-25 11:45:39 +0200

    stuff

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,20 @@
 module PostsHelper
+  def videomatcher(content)
+    if content.match('\[video:\s?\S+\s?\]') 
+      fullcontent = content.match('\[video:\s?[^ ]+?\]')
+      video_id = content.match('\[video:\s?(?<youtube_id>\w+)\]') 
+      full_link = '</span></p><embed width="420" height="345" src="http://www.youtube.com/v/'
+       + video_id[:youtube_id]  + 
+       '" type="application/x-shockwave-flash" allowfullscreen="true"></embed><p><span> ' 
+      if fullcontent.length > 1 
+        for e in fullcontent 
+         video = prepare_v.gsub(e, full_link) 
+        end 
+      else 
+        video = prepare_v.gsub(fullcontent[0], full_link) 
+      end 
+    else
+      video = content
+    end 
+    return video
 end
