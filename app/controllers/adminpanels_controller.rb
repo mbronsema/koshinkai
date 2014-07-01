591acc3cb01a97d7353f10b59af9165c6243003e
Martin <martin.bronsema@gmail.com>
2014-07-01 12:38:31 +0200

    adminpanelstuff

--- app/controllers/adminpanels_controller.rb
+++ app/controllers/adminpanels_controller.rb
@@ -1,8 +1,10 @@
 class AdminpanelsController < ApplicationController
+  before_filter :authenticate_user!
   def index
     @events = Event.all
     @users = User.all
     @posts = Post.all
+    @categories = Category.all
   end
 end
 
